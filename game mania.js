b3bfe50cf03eb8cb0969594a4eb9c0a1efbd3362
sparsh0602 <75162752+sparsh0602@users.noreply.github.com>
2021-10-29 01:47:19 +0530

    Update game mania.js

--- game mania.js
+++ game mania.js
@@ -13,3 +13,64 @@ function SlideShow() {
   setTimeout(SlideShow, 2000); // Change image every 2 seconds
 } 
 
+
+
+ document.getElementsByTagName('select')[0].addEventListener('change',function(){ 
+  var val = document.getElementsByTagName('select')[0].value; 
+ 
+  
+  if(val==="SNAKE MANIA")
+  {
+  let a=document.querySelector("#snake1");
+  let b=document.querySelector("#snake2");
+  let c=document.querySelector("#snake3");
+  let d=document.querySelector("#snake4");
+
+   a.style.display="block";
+   b.style.display="none";
+   c.style.display="none";
+   d.style.display="none";
+    
+  }
+
+  if(val==="TIC TAC TOE")
+  {
+   let a=document.querySelector("#snake1");
+   let b=document.querySelector("#snake2");
+   let c=document.querySelector("#snake3");
+   let d=document.querySelector("#snake4");
+
+   a.style.display="none";
+   b.style.display="none";
+   c.style.display="block";
+   d.style.display="none";
+     
+    }
+
+    if(val==="DINOSAUR MANIA")
+    {
+   let a=document.querySelector("#snake1");
+   let b=document.querySelector("#snake2");
+   let c=document.querySelector("#snake3");
+   let d=document.querySelector("#snake4");
+
+   a.style.display="none";
+   b.style.display="block";
+   c.style.display="none";
+   d.style.display="none";
+    }
+   
+    if(val==="FLIP IT")
+    {
+   let a=document.querySelector("#snake1");
+   let b=document.querySelector("#snake2");
+   let c=document.querySelector("#snake3");
+   let d=document.querySelector("#snake4");
+
+   a.style.display="none";
+   b.style.display="none";
+   c.style.display="none";
+   d.style.display="block";
+    }
+ }); 
+
